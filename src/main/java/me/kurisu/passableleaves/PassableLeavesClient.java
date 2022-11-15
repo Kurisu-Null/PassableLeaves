5ed7879c9c6b1ee03d9b637e80a255ee3f98484f
Kurisu <k3yj1n@gmail.com>
2022-11-15 21:27:51 +0100

    fix: cloth-config dep & crash, project structure

--- src/main/java/me/kurisu/passableleaves/PassableLeavesClient.java
+++ src/main/java/me/kurisu/passableleaves/PassableLeavesClient.java
@@ -5,12 +5,15 @@ import me.lortseam.completeconfig.gui.cloth.ClothConfigScreenBuilder;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.loader.api.FabricLoader;
 
 @Environment(EnvType.CLIENT)
 public class PassableLeavesClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        ConfigScreenBuilder.setMain(PassableLeaves.MOD_ID, new ClothConfigScreenBuilder());
+        if (FabricLoader.getInstance().isModLoaded("cloth-config")) {
+            ConfigScreenBuilder.setMain(PassableLeaves.MOD_ID, new ClothConfigScreenBuilder());
+        }
     }
 
 }
